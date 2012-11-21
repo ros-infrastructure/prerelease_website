989578e4ea2d082f099bbf7a71cba2e554c507e2
Eitan Marder-Eppstein <eitan@hidof.com>
2012-11-21 13:31:42 -0800

    Website that will generate a rosinstall file

--- prerelease_website/urls.py
+++ prerelease_website/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -22,3 +23,7 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += patterns('',
+                        (r'^media/(?P<path>.*)$',
+                         'django.views.static.serve', {
+                             'document_root': settings.MEDIA_ROOT}))
