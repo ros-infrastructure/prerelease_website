363d570e4d63671ca497dee90468dac07c54444c
Dirk Thomas <dthomas@osrfoundation.org>
2013-07-02 16:45:31 -0700

    refactor rosinstall_gen to use pthon rosinstall_generator library

--- prerelease_website/rosinstall_gen/ajax.py
+++ prerelease_website/rosinstall_gen/ajax.py
@@ -4,8 +4,7 @@ from django.utils import simplejson
 import datetime
 import os
 import yaml
-from prerelease_website.rosinstall_gen.distro import generate_rosinstall
-from prerelease_website.rosinstall_gen.old_distro import generate_dry_rosinstall
+from prerelease_website.rosinstall_gen.views import get_rosinstall
 
 import logging
 logger = logging.getLogger('submit_jobs')
@@ -13,6 +12,19 @@ logger = logging.getLogger('submit_jobs')
 @dajaxice_register
 def get_rosinstall_ajax(request, distro, packages, gen_type):
     logger.info("Got distro %s, packages %s, gen_type %s" % (distro, packages, gen_type))
+
+    try:
+        if gen_type == 'dry':
+            logger.info("Calling dry")
+            rosinstall = get_rosinstall(distro, packages, dry_only=True)
+        elif gen_type == 'combined':
+            rosinstall = get_rosinstall(distro, packages)
+        else:
+            logger.info("Calling wet")
+            rosinstall = get_rosinstall(distro, packages, wet_only=True)
+    except RuntimeError as e:
+        return simplejson.dumps({'rosinstall_error': 'Error generating rosinstall file: {0}'.format(e)})
+
     timestr = datetime.datetime.now().strftime("%Y%m%d-%H%M%S%f")
     filename = '%s.rosinstall' % '_'.join(packages)
     relative_dir = os.path.join('rosinstalls', timestr)
@@ -22,22 +34,8 @@ def get_rosinstall_ajax(request, distro, packages, gen_type):
         os.makedirs(os.path.realpath(os.path.dirname(path)))
     except:
         pass
-
-    if gen_type == 'dry':
-        logger.info("Calling dry")
-        rosinstall = generate_dry_rosinstall(distro, packages[0])
-    elif gen_type == 'combined':
-        logger.info("Calling combined")
-        dry_rs = generate_dry_rosinstall(distro, packages[0])
-        wet_rs = generate_rosinstall(distro, packages[0])
-        rosinstall = dry_rs + wet_rs
-    else:
-        logger.info("Calling wet")
-        rosinstall = generate_rosinstall(distro, packages)
-
     with open(path, 'w+') as f:
         f.write(rosinstall)
-        #yaml.dump(rosinstall, f, default_flow_style=False)
 
     return simplejson.dumps({'rosinstall_url': os.path.join(relative_dir,
                                                             filename)})
--- prerelease_website/rosinstall_gen/views.py
+++ prerelease_website/rosinstall_gen/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseServerError
-from prerelease_website.rosinstall_gen.distro import generate_rosinstall
-from prerelease_website.rosinstall_gen.old_distro import generate_dry_rosinstall
+import yaml
+
+from rosinstall_generator.generator import generate_rosinstall, sort_rosinstall
 
 import logging
 logger = logging.getLogger('submit_jobs')
+
 
 # Create your views here.
 def landing_page(request):
     return render_to_response('landing_page.html', {})
 
-def dry_raw(request, distro, variant):
-    rosinstall = generate_dry_rosinstall(distro, variant)
+def dry_raw(request, distro, packages):
+    try:
+        rosinstall = get_rosinstall(distro, packages.split(','), dry_only=True)
+    except RuntimeError as e:
+        return HttpResponseServerError("Error generating rosinstall file: {0}".format(e))
     return render_to_response('rosinstall.html', {'rosinstall': rosinstall})
 
 def dry_index(request, distro, packages):
@@ -25,8 +30,8 @@ def dry_index(request, distro, packages):
                                              context_instance=RequestContext(request))
 def raw(request, distro, packages):
     try:
-        rosinstall = generate_rosinstall(distro, packages.split(','))
-    except Exception as e:
+        rosinstall = get_rosinstall(distro, packages.split(','), wet_only=True)
+    except RuntimeError as e:
         return HttpResponseServerError("Error generating rosinstall file: {0}".format(e))
     return render_to_response('rosinstall.html', {'rosinstall': rosinstall})
 
@@ -38,11 +43,12 @@ def index(request, distro, packages):
                                              'gen_type': 'wet'},
                                              context_instance=RequestContext(request))
 
-def combined_raw(request, distro, variant):
-    dry_rs = generate_dry_rosinstall(distro, variant)
-    wet_rs = generate_rosinstall(distro, variant)
-    combined = dry_rs + wet_rs
-    return render_to_response('rosinstall.html', {'rosinstall': combined})
+def combined_raw(request, distro, packages):
+    try:
+        rosinstall = get_rosinstall(distro, packages.split(','))
+    except RuntimeError as e:
+        return HttpResponseServerError("Error generating rosinstall file: {0}".format(e))
+    return render_to_response('rosinstall.html', {'rosinstall': rosinstall})
 
 def combined_index(request, distro, packages):
     logger.info('Distro: %s' % distro)
@@ -52,3 +58,7 @@ def combined_index(request, distro, packages):
                                              'gen_type': 'combined'},
                                              context_instance=RequestContext(request))
 
+def get_rosinstall(distro_name, names, wet_only=False, dry_only=False):
+    rosinstall_data = generate_rosinstall(distro_name, names, wet_only=wet_only, dry_only=dry_only, deps=True, tar=True)
+    rosinstall_data = sort_rosinstall(rosinstall_data)
+    return yaml.safe_dump(rosinstall_data, default_flow_style=False)
