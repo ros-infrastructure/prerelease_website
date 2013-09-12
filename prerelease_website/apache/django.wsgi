985053daa204ba0305fc002ced291ded706b2f70
Dirk Thomas <dthomas@osrfoundation.org>
2013-09-12 16:37:44 -0700

    find scripts and invoke with absolute path

--- prerelease_website/apache/django.wsgi
+++ prerelease_website/apache/django.wsgi
@@ -1,11 +1,8 @@
 import os
-import sys
- 
-path = '/var/www/prerelease_website'
-if path not in sys.path:
-    sys.path.insert(0, path)
- 
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'prerelease_website.settings'
- 
+os.environ['PATH'] = '/var/www/prerelease_website/venv/bin%s%s' % (os.path.pathsep, os.getenv('PATH'))
+os.environ['ROS_HOME'] = '/var/www/prerelease_website'
+
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
--- prerelease_website/submit_jobs/ajax.py
+++ prerelease_website/submit_jobs/ajax.py
@@ -2,12 +2,11 @@ from dajax.core import Dajax
 from dajaxice.decorators import dajaxice_register
 from django.utils import simplejson
 import logging
+import os
 import subprocess
 from models import WetRosDistro, DryRosDistro
 
-
 logger = logging.getLogger('submit_jobs')
-
 
 
 @dajaxice_register
@@ -27,8 +26,6 @@ def get_repo_list_ajax(request, ros_distro):
    return simplejson.dumps({'repo_list': repo_list})
 
 
-
-
 @dajaxice_register
 def run_jobs_ajax(request, email, ros_distro, repo_list):
    logger.info("---")
@@ -43,14 +40,22 @@ def run_jobs_ajax(request, email, ros_distro, repo_list):
       f = open('/var/www/prerelease_website/jenkins.conf')
       info = f.read().split(',')
 
-      command = 'generate_groovy_prerelease.py %s %s --repeat 0 --email %s --rosdistro %s'%(info[0], info[1], email, ros_distro)
+      script = find_executable('generate_groovy_prerelease.py')
+      if not script:
+         logger.error('Could not find generate_groovy_prerelease.py script')
+         assert False
+      command = '%s %s %s --repeat 0 --email %s --rosdistro %s'%(script, info[0], info[1], email, ros_distro)
 
       for r in repo_list:
          command += " --stack %s"%r
 
    elif '_wet' in ros_distro:
       ros_distro = ros_distro.split("_")[0]
-      command = "generate_jenkins_prerelease %s %s %s"%(email, ros_distro, ' '.join(['%s %s'%(r, v) for r, v in repo_list.iteritems()]))
+      script = find_executable('generate_jenkins_prerelease')
+      if not script:
+         logger.error('Could not find generate_jenkins_prerelease script')
+         assert False
+      command = "%s %s %s %s"%(script, email, ros_distro, ' '.join(['%s %s'%(r, v) for r, v in repo_list.iteritems()]))
 
    else:
       assert False, 'Neither wet nor dry'
@@ -77,5 +82,9 @@ def run_jobs_ajax(request, email, ros_distro, repo_list):
    return simplejson.dumps({'success': success, 'command': command, 'std_out': res, 'std_err': err})
 
 
-
-
+def find_executable(file_name):
+    for path in os.getenv('PATH').split(os.path.pathsep):
+        file_path = os.path.join(path, file_name)
+        if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+            return file_path
+    return None
